63fbe1a446acfcb772ff185d7e80b1541b52dd0c
H1r0Kazu <miyata.hirokazu@gmail.com>
2019-11-26 20:49:49 +0900

    移動１の実行

--- 8th/flyght_test_hm_02.py
+++ 8th/flyght_test_hm_02.py
@@ -2,19 +2,16 @@
 # 講習名： ドローンソフトウェアエンジニア
 # 記載日：　2019/11/25
 # 作成者：　宮田浩和
-# ファイル名：　flyght_test_hm_01.py
-# 内容　：　Day3のテキストを元に、離陸 → 目標高度 に移動する。
-
+# ファイル名：　flyght_test_hm_02.py
+# 内容　：　離陸後にちょっとだけ移動する。 テキストの 移動１ の部分
 #
 
-# Day3に書いたコード
-# シャープマークから始まる行はコメントです。
-# プログラムとして実行対象外です。
 
 # 使用する関数、クラスを宣言する
 from dronekit import connect, VehicleMode
 # 標準ライブラリのtimeを使う宣言
 import time
+
 
 # 機体に接続する
 # シミュレータに接続する場合
@@ -22,10 +19,25 @@ vehicle = connect('127.0.0.1:14550', wait_ready=True)
 # UbuntuでUSB経由で接続する場合
 # vehicle = connect('/dev/ttyACM0,115200', wait_ready=True)
 
+
 # アーミング可能かチェック
 while not vehicle.is_armable:
     print "Waiting for vehicle to initializ)e…"
     time.sleep(1)    
+
+
+# vehicle.home_locationに値がセットされるまで
+# downloadを繰り返し実行する。
+while not vehicle.home_location:
+    cmds = vehicle.commands
+    cmds = download()
+    cmds = wait_ready()
+
+    if not vehicle.home_location:
+        print "Waiting for home location ...."
+
+# ホームロケーションの取得完了
+print "\n Home location: %s " % vehicle.home_location
 
 
 # アーミングの実行
@@ -40,13 +52,20 @@ while not vehicle.armed:
     print "Waiting for arming..."
     time.sleep(1)
 
+
+# 目標の緯度・経度・高度を設定する
+aLocation = LocationGlobalRelative(35.8787491, 140.3393722, 30)
+
+
 # 目標高度を設定
 targetAltitude = 20
+
 
 # テイクオフ実行
 # 20mの高さまで離陸する
 print "Take Off!!"
 vehicle.simple_takeoff(targetAltitude)
+
 
 # 目標の高度に達するまで待つ
 while True:
@@ -56,6 +75,10 @@ while True:
         break
         
     time.sleep(1)
+
+
+# simele_goto を実行する
+vehicle.simple_goto
 
 
 # ここでプログラム終了
