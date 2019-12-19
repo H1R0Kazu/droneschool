dc4df4efa5176f5eafcef97560ac2a15bcaee529
H1r0Kazu <miyata.hirokazu@gmail.com>
2019-12-20 00:07:39 +0900

    座標指定移動の項目を追加

--- 8th/flyght_test_hm_03.py
+++ 8th/flyght_test_hm_03.py
@@ -10,14 +10,15 @@ from dronekit import connect, VehicleMode
 # 標準ライブラリのtimeを使う宣言
 import time
 
+
 # 機体に接続する
 # シミュレータに接続
 vehicle = connect('127.0.0.1:14550', wait_ready=True)
 
 # アーミング可能かチェック
 while not vehicle.is_armable:
-    print "Waiting for vehicle to initializ)e…"
-    time.sleep(1)    
+    print "Waiting for vehicle to initialize"
+    time.sleep(1)
 
 # vehicle.home_locationに値がセットされるまで
 # downloadを繰り返し実行する
@@ -32,36 +33,59 @@ while not vehicle.home_location:
 # ホームロケーションの取得完了
 print "\n Home location: %s " % vehicle.home_location
 
-# vehicle is an instance of the Vehicle class
-print "Autopilot Firmware version: %s" % vehicle.version
-# print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
-print "Global Location: %s" % vehicle.location.global_frame
-print "Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
-print "Local Location: %s" % vehicle.location.local_frame    #NED
-print "Attitude: %s" % vehicle.attitude
-print "Velocity: %s" % vehicle.velocity
-print "GPS: %s" % vehicle.gps_0
-print "Groundspeed: %s" % vehicle.groundspeed
-print "Airspeed: %s" % vehicle.airspeed
-print "Gimbal status: %s" % vehicle.gimbal
-print "Battery: %s" % vehicle.battery
-print "EKF OK?: %s" % vehicle.ekf_ok
-print "Last Heartbeat: %s" % vehicle.last_heartbeat
-print "Rangefinder: %s" % vehicle.rangefinder
-print "Rangefinder distance: %s" % vehicle.rangefinder.distance
-print "Rangefinder voltage: %s" % vehicle.rangefinder.voltage
-print "Heading: %s" % vehicle.heading
-print "Is Armable?: %s" % vehicle.is_armable
-print "System status: %s" % vehicle.system_status.state
-print "Mode: %s" % vehicle.mode.name    # settable
-print "Armed: %s" % vehicle.armed    # settable
+# アーミング実行
+# フライトモードを”GUIDED”に変更し、armedにTrueを設定する。
+print "Arming Moters"
+vehicle.mode = vehicleMode("GUIDED")
+vehicle.armd = True
 
-# 
-print "\n Class key and Value."
-for key, value in vehicle.location.global_frame.__dict__.items():
-	print(key, value)
+while not vehicle.armed:
+    print "Waiting for arming..."
+    time.sleep(1)
+
+# 目標高度を設定
+targetAltitude = 20
+
+# テイクオフの実行
+# 20mの高さまで離陸する
+print "Take Off!!"
+vehicle.simple_takeoff(targetAltitude)
+
+# 目標の高度に達するまで待つ
+while True:
+    print "Altitude: ", vehicle.location.global_relative_frame.alt 
+
+    if vehicle.location.global_relative_frame.alt >= targetAltitude * 0.95:
+        print "Reached target altitude"
+        break
+
+    time.sleep(1)
+    
+
+# 座標指定で移動
+# 目標ロケーションの設定 北に移動
+target1_location_lat = vehicle.location.global_relative_frame.lat + 0.00001
+target1_location_lon = vehicle.location.global_relative_frame.lon
+target1_location_alt = 30
+
+aLocation = LocationGlobalRelative(target1_location_lat, target1_location_lon, target1_location_alt)
+
+vehicle.simple_goto(aLocation)
+
+# 目標のロケーションに達するまで待つ
+while True:
+    print "Altitude: ", vehicle.location.global_relative_frame.alt 
+    # ロケーション情報の表示
+    print "\n Current location: %s " % vehicle.location.global_relative_frame
+
+    if vehicle.location.global_relative_frame.lat >= target1_location_lat  * 0.95:
+        print "Reached target lat"
+        break
+
+    time.sleep(1)
 
 
-print(vehicle.location.global_frame.lat)
+
+
 
 # プログラム終わり
